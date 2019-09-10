48e0bce50dcd2056deb444e02964c2ab48c9c483
Woohyeok Aaron Kim <woohyeok.kim@rakuten.com>
2019-09-10 10:46:10 +0900

    Initial commit : base

---
+++ src/main/java/com/mmgo/cashlessmap/ExecuteCashLessMap.java
@@ -0,0 +1,13 @@
+package com.mmgo.cashlessmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExecuteCashLessMap {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExecuteCashLessMap.class, args);
+	}
+
+}
---
+++ src/test/java/com/mmgo/cashlessmap/ExecuteCashLessMapTest.java
@@ -0,0 +1,29 @@
+package com.mmgo.cashlessmap;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExecuteCashLessMapTest {
+
+	@Before
+	public void setUp() {
+
+	}
+
+	@After
+	public void tearDown() {
+		
+	}
+
+	@Test
+	public void runAppTest() {
+		
+	}
+
+}
