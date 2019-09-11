d84fe3590556de2b2f27e0d386c4179bebe8e515
Woohyeok Aaron Kim <woohyeok.kim@rakuten.com>
2019-09-11 15:39:26 +0900

    Fixed

--- src/main/resources/static/js/select.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $("#rakutenicon").click(function() {
-        console.log("ok");
-        var src = $(this).children('img').attr('src');
-        $("#selectedicon").attr("src",src);
-        console.log(src);
-        
-    });
-});
