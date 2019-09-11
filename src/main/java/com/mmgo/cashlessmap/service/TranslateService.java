a572fca7803758dffad2e1c842d5e21a6a36c7b1
Miyake, Ko a | Ko | ECFD <ko.a.miyake@rakuten.com>
2019-09-11 17:04:17 +0900

    don't use translate when target lang is jp

--- src/main/java/com/mmgo/cashlessmap/service/TranslateService.java
+++ src/main/java/com/mmgo/cashlessmap/service/TranslateService.java
@@ -6,8 +6,11 @@ import java.net.URISyntaxException;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonSyntaxException;
+import com.mmgo.cashlessmap.entity.Store;
+import com.mmgo.cashlessmap.entity.Stores;
 import com.mmgo.cashlessmap.entity.Translate;
 
+import com.mmgo.cashlessmap.utility.Option;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpException;
 import org.apache.http.HttpStatus;
@@ -17,7 +20,6 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,12 +27,33 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class TranslateService {
 
-    private String credential =  "AIzaSyAEuceCXpy1UCZs9J6ic-XHtSafbntDFeA\n";
+    private String credential =  "AIzaSyAEuceCXpy1UCZs9J6ic-XHtSafbntDFeA";
     
     private Gson gson = new Gson();
 
-    public String translate(Translate todo) throws JsonSyntaxException, ParseException, IOException, HttpException {
-        try (CloseableHttpResponse response = HttpClients.createDefault().execute(createQueryHttpPost(todo));) {
+    public Stores translate(Stores stores, Option option) {
+      try {
+        for(Store store : stores.getStores()) {
+          Translate translate = new Translate(store.name, option.lang);
+          store.translatedName = translate(translate);
+          translate.setText(store.prLong);
+          store.translatedPrLong = translate(translate);
+          translate.setText(store.prShort);
+          store.translatedPrShort = translate(translate);
+        }
+      } catch (IOException e) {
+        e.printStackTrace();
+      } catch (HttpException e) {
+        e.printStackTrace();
+      }
+      return stores;
+    }
+
+    public String translate(Translate translate) throws JsonSyntaxException, ParseException, IOException, HttpException {
+        if(translate.getTargetLanguage().equals("ja")) {
+          return translate.getText();
+        }
+        try (CloseableHttpResponse response = HttpClients.createDefault().execute(createQueryHttpPost(translate));) {
             int statusCode = response.getStatusLine().getStatusCode();
             if (statusCode == HttpStatus.SC_OK) {
                 return parseTranslationText(response.getEntity());
@@ -43,17 +66,17 @@ public class TranslateService {
     private HttpPost createQueryHttpPost(Translate todo) {
         
     	URIBuilder builder = null;
-		try {
-			builder = new URIBuilder("https://translation.googleapis.com/language/translate/v2");
-		} catch (URISyntaxException e) {
-			e.printStackTrace();
-		}
-    	builder.setParameter("q", todo.getText()).setParameter("target", todo.getTargetLanguage()).setParameter("key",credential);
-    	try {
-			return new HttpPost(builder.build());
-		} catch (URISyntaxException e) {
-			e.printStackTrace();
-		}
+      try {
+        builder = new URIBuilder("https://translation.googleapis.com/language/translate/v2");
+      } catch (URISyntaxException e) {
+        e.printStackTrace();
+      }
+      builder.setParameter("q", todo.getText()).setParameter("target", todo.getTargetLanguage()).setParameter("key",credential);
+      try {
+        return new HttpPost(builder.build());
+      } catch (URISyntaxException e) {
+        e.printStackTrace();
+      }
     	return null;
     }
     
@@ -68,5 +91,4 @@ public class TranslateService {
             .get("translatedText")
             .getAsString();
     }
-
 }
